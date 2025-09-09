a05a69c4caab322ef7335ab6a8764ac90d6e9c08
swiftcynic <dhruv.salot@outlook.com>
2025-09-09 13:23:26 +1000

    Initial Commit

---
+++ next/app/[locale]/(marketing)/ClientSlugHandler.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+import { useSlugContext } from '@/app/context/SlugContext';
+
+export default function ClientSlugHandler({
+  localizedSlugs,
+}: {
+  localizedSlugs: Record<string, string>;
+}) {
+  const { dispatch } = useSlugContext();
+
+  useEffect(() => {
+    if (localizedSlugs) {
+      dispatch({ type: 'SET_SLUGS', payload: localizedSlugs });
+    }
+  }, [localizedSlugs, dispatch]);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleMessage = async (message: MessageEvent<any>) => {
+      if (
+        message.origin === process.env.NEXT_PUBLIC_API_URL &&
+        message.data.type === 'strapiUpdate'
+      ) {
+        router.refresh();
+      }
+    };
+
+    // Add the event listener
+    window.addEventListener('message', handleMessage);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, [router]);
+
+  return null; // This component only handles the state and doesn't render anything.
+}
